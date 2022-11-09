3baf2e6f80e92bae998e218e09e83b382cfc50bc
Akash Chanara <akashchanara@gmail.com>
2022-11-09 13:07:13 -0800

    get router for all

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -5,4 +5,23 @@ const withAuth = require("../../utils/auth");
 
 router.get("/", (req, res) => {
     console.log("======================");
-});
+    Post.findAll({
+      attributes: ["id", "title", "img_url"],     
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Tag,
+          attributes: ["id", "tag_title"],
+          
+        },
+      ],
+    })
+      .then((PostData) => res.json(PostData.reverse()))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
