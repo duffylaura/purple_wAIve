7a6ed310f764b3a0299d99c250f79ad0f84375b6
Akash Chanara <akashchanara@gmail.com>
2022-11-09 13:13:36 -0800

    router for single id

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -25,3 +25,38 @@ router.get("/", (req, res) => {
         res.status(500).json(err);
       });
   });
+// post for single id
+router.get("/:id", (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "title", "img_url"],
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Tag,
+          attributes: ["id", "tag_title"],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+      ],
+    })
+      .then((PostData) => {
+        if (!PostData) {
+          res.status(404).json({ message: "No post found with this id" });
+          return;
+        }
+        res.json(PostData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
