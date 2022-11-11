373e8cb057ce6f21c14ecd2c6c7e6daf362805de
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-11-10 17:00:15 -0800

    functional keyword tag generation and reset"

--- public/js/create.js
+++ public/js/create.js
@@ -4,9 +4,21 @@ const postStyle = document.querySelector('#style');
 const postBody = document.querySelector('#postBody');
 const addTagButton = document.querySelector('#add-tag');
 const tagDisp = document.querySelector('#tags-display');
+const createForm = document.querySelector('#create-form');
+const resetBtn = document.querySelector('#reset-tag')
+let keyword = [];
 
-const creatPostForm = async (e) => {
+
+const createPostForm = async (e) => {
     e.preventDefault();
+
+    const newTitle = postTitle.value;
+    const newBody = postBody.value;
+    const newStyle = postStyle.value;
+    const newKeywords = keyword.join(" ");
+    console.log(newKeywords);
+    console.log(newStyle);
+
 
 
 
@@ -16,20 +28,35 @@ const creatPostForm = async (e) => {
 
 
 }
-//logic to add tag to page for action feedback;
+
+//logic to add tag to page for action feedback and to keep track of our keywords;
 const addTag = (e) => {
+    let i = 1
     e.preventDefault();
     //taking a hold of the value 
-    let tag = postTag.value;
+    let tag = postTag.value.trim();
+    keyword.push(tag);
+    console.log(keyword)
     //making new container to store that, with bulma class
     let newTag = document.createElement("span");
     newTag.classList.add("tag", "is-warning", "is-medium");
     //adding the content
-    newTag.innerHTML = tag;
+    newTag.innerHTML = `${tag}`;
     //appending to display;
     tagDisp.append(newTag);
+    //clearing input field
+    postTag.value = "";
 };
 
-addTagButton.addEventListener("click", addTag);
+const resetTag = (e) => {
+    e.preventDefault();
 
+    keyword = [];
+    postTag.value = "";
+    tagDisp.innerHTML = "<p>KeyWords to be used: </p>"
+};
+
+resetBtn.addEventListener("click", resetTag);
+addTagButton.addEventListener("click", addTag);
+createForm.addEventListener("submit", createPostForm);
 
